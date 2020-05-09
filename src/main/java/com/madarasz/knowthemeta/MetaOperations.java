639b931987eb1e4797112acdde892e0a8ff58ae5
madarasz <mr.madarasz.istvan@gmail.com>
2020-05-09 11:54:19 +0200

    big MetaOperations refactor

--- src/main/java/com/madarasz/knowthemeta/MetaOperations.java
+++ src/main/java/com/madarasz/knowthemeta/MetaOperations.java
@@ -2,7 +2,6 @@ package com.madarasz.knowthemeta;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 
 import com.madarasz.knowthemeta.brokers.ABRBroker;
@@ -26,6 +25,7 @@ import com.madarasz.knowthemeta.database.DRs.MetaRepository;
 import com.madarasz.knowthemeta.database.DRs.StandingRepository;
 import com.madarasz.knowthemeta.database.DRs.TournamentRepository;
 import com.madarasz.knowthemeta.database.DRs.UserRepository;
+import com.madarasz.knowthemeta.helper.Searcher;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,6 +34,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StopWatch;
 
+/**
+ * Class for operations related to Meta entities.
+ */
 @Service
 public class MetaOperations {
 
@@ -50,10 +53,24 @@ public class MetaOperations {
     @Autowired StandingRepository standingRepository;
     @Autowired UserRepository userRepository;
     @Autowired DeckRepository deckRepository;
+    @Autowired Searcher searcher;
 
     private static final Logger log = LoggerFactory.getLogger(MetaOperations.class);
     private static final StopWatch stopwatch = new StopWatch();
+    private int tournamentCreatedCount;
+    private int standingCreatedCount;
+    private int matchUpdatedCount ;
+    private int userCreatedCount;
+    private int deckCreatedCount;
 
+    
+    /** 
+     * Adds Meta entity
+     * @param mwlCode mwlCode for MWL
+     * @param packCode packCode of cardpool
+     * @param newCards does meta contain new cards
+     * @param title meta title
+     */
     @Transactional
     public void addMeta(String mwlCode, String packCode, Boolean newCards, String title) {
         // TODO: stop if meta exists with same name
@@ -66,6 +83,11 @@ public class MetaOperations {
         log.info(String.format("New meta added: %s (%s - %s)", title, packCode, mwlCode));
     }
 
+    
+    /**
+     * Deletes meta with that title  
+     * @param title
+     */
     @Transactional
     public void deleteMeta(String title) {
         Meta meta = metaRepository.findByTitle(title);
@@ -77,102 +99,171 @@ public class MetaOperations {
         }
     }
 
+    
+    /**
+     * Loads tournament, standing, deck, player and match data for a meta and persists it into DB. 
+     * @param meta
+     * @return String update message
+     */
     @Transactional
     public String getMetaData(Meta meta) {
+        // init
         stopwatch.start();
-        int tournamentCreatedCount = 0;
-        int standingCreatedCount = 0;
-        int matchUpdatedCount = 0;
-        int userCreatedCount = 0;
-        int deckCreatedCount = 0;
-        Long metaId = meta.getId();
+        tournamentCreatedCount = 0;
+        standingCreatedCount = 0;
+        matchUpdatedCount = 0;
+        userCreatedCount = 0;
+        deckCreatedCount = 0;
 
+        // load data
         Set<CardInPack> identities = cardInPackRepository.listIdentities();
         Set<CardInPack> cards = cardInPackRepository.listAll();
-        List<Tournament> existingTournaments = tournamentRepository.listForMeta(metaId);
-        log.info("Existing tournaments for meta: "+existingTournaments.size());
+        List<Tournament> existingTournaments = tournamentRepository.listForMeta(meta.getId());
         List<User> existingUsers = userRepository.listAll();
         Set<Deck> existingDecks = deckRepository.listAll();
         List<Tournament> tournaments = abrBroker.getTournamentData(meta);
+        log.info("*** Starting meta update for " + meta.getTitle());
+        log.info(String.format("Existing tournaments for meta: %d", existingTournaments.size()));
 
+        // iterate on ABR data
         for (Tournament tournament : tournaments) {
             // tournament
             log.trace(String.format("Looking at tournament %s (#%d)", tournament.getTitle(), tournament.getId()));
-            int tournamentId = tournament.getId();
-            tournamentCreatedCount += updateTournaments(existingTournaments, tournament);
+            updateTournaments(existingTournaments, tournament);
             // standings
-            List<Standing> standings = abrBroker.getStadingData(tournament, identities, cards, existingDecks);
-            Set<Standing> existingStandings = standingRepository.findByTournament(tournamentId);
-            for (Standing standing : standings) {
-                // decks
-                if (standing.getDeck() != null) {
-                    Deck deck = standing.getDeck();     
-                    // player
-                    userCreatedCount += updateWithPlayer(existingUsers, deck);
-                    // deck
-                    deckCreatedCount += updateWithDeck(existingDecks, standing, deck);
-                }
-                Optional<Standing> existingStanding = existingStandings.stream().filter(x -> x.getRank() == standing.getRank() && x.getIsRunner() == standing.getIsRunner()).findFirst();
-                if (!existingStanding.isPresent()) {
-                    // new standing
-                    standingRepository.save(standing);
-                    existingStandings.add(standing);
-                    standingCreatedCount++;
-                }
-            }
-            // matches
-            if (tournament.isMatchDataAvailable()) {
-                matchUpdatedCount += updateStandingsWithMatchData(tournamentId, existingStandings);
-            }
+            updateStandings(tournament, identities, cards, existingDecks, existingUsers);
         }
-        // update counters
+        // update meta counters
         updateMetaCounts(meta);
 
         // logging
         stopwatch.stop();
         String message = String.format("Meta update \"%s\" finished (%.3f sec) - New tournament: %d, new stading: %d, new deck: %d, new player: %d, match updates: %d", 
             meta.getTitle(), stopwatch.getTotalTimeSeconds(), tournamentCreatedCount, standingCreatedCount, deckCreatedCount, userCreatedCount, matchUpdatedCount);
-        log.info(message);
+        log.info("*** " + message);
         return message;
     }
 
-    private int updateTournaments(List<Tournament> existingTournaments, Tournament tournament) {
+    
+    /** 
+     * Saves tournament to DB if it did not exist there. Increments tournamentCreatedCount counter.
+     * @param existingTournaments list of existing tournaments in DB
+     * @param tournament tournament in question
+     */
+    private void updateTournaments(List<Tournament> existingTournaments, Tournament tournament) {
         int tournamentId = tournament.getId();
         if (!existingTournaments.stream().filter(x -> x.getId() == tournamentId).findFirst().isPresent()) {
             // new tournament
             log.debug("New tournament saved: " + tournament.toString());
             tournamentRepository.save(tournament);
             existingTournaments.add(tournament);
-            return 1;
+            tournamentCreatedCount++;
         }
-        return 0;
     }
 
-    private int updateWithPlayer(List<User> existingUsers, Deck deck) {
+    
+    /** 
+     * @param tournament
+     * @param identities
+     * @param cards
+     * @param existingDecks
+     * @param existingUsers
+     */
+    private void updateStandings(Tournament tournament, Set<CardInPack> identities, Set<CardInPack> cards, Set<Deck> existingDecks, List<User> existingUsers) {
+        int tournamentId = tournament.getId();
+        List<Standing> standings = abrBroker.getStadingData(tournament, identities, cards, existingDecks);
+        Set<Standing> existingStandings = standingRepository.findByTournament(tournamentId);
+        for (Standing standing : standings) {
+            // decks
+            if (standing.getDeck() != null) {
+                Deck deck = standing.getDeck();     
+                // player
+                updateDeckWithPlayer(existingUsers, deck);
+                // deck
+                updateStandingWithDeck(existingDecks, standing, deck);
+            }
+            Standing existingStanding = searcher.getStadingByRankSide(existingStandings, standing.getRank(), standing.getIsRunner());
+            if (existingStanding == null) {
+                // new standing
+                standingRepository.save(standing);
+                existingStandings.add(standing);
+                standingCreatedCount++;
+            }
+        }
+        // matches
+        if (tournament.isMatchDataAvailable()) {
+            updateStandingsWithMatchData(tournamentId, existingStandings);
+        }
+    }
+
+    
+    /**
+     * Updates deck with its user. 
+     * Checks if user exists in DB, if not, it creates it.
+     * Increments userCreatedCount counter. 
+     * @param existingUsers list of existing users in DB
+     * @param deck deck in question
+     */
+    private void updateDeckWithPlayer(List<User> existingUsers, Deck deck) {
         int userId = deck.getPlayer().getUser_id();
-        Optional<User> existingPlayer = existingUsers.stream().filter(x -> x.getUser_id() == userId).findFirst();
-        if (!existingPlayer.isPresent() && userId > 0) {
+        User existingPlayer = searcher.getUserById(existingUsers, userId);
+        if (existingPlayer == null && userId > 0) { // undefined users have userID=0 in JSON
             User player = deck.getPlayer();
             userRepository.save(player);
             existingUsers.add(player);
-            return 1;
-        } 
-        deck.setPlayer(existingPlayer.get());
-        return 0;
+            userCreatedCount++;
+        } else {
+            deck.setPlayer(existingPlayer);
+        }
     }
 
-    private int updateWithDeck(Set<Deck> existingDecks, Standing standing, Deck deck) {
-        int deckId = deck.getId();
-        Optional<Deck> existingDeck = existingDecks.stream().filter(x -> x.getId() == deckId).findFirst();
-        if (!existingDeck.isPresent()) {
+    
+    /**
+     * Updates standing with its deck.
+     * Checks if deck is in DB, if not, it creates it.
+     * Increaments deckCreatedCount counter.
+     * @param existingDecks list of existing decks in db
+     * @param standing related stading of deck
+     * @param deck deck in question
+     */
+    private void updateStandingWithDeck(Set<Deck> existingDecks, Standing standing, Deck deck) {
+        Deck existingDeck = searcher.getDeckById(existingDecks, deck.getId());
+        if (existingDeck == null) {
             deckRepository.save(deck);
             existingDecks.add(deck);
-            return 1;
-        } 
-        standing.setDeck(existingDeck.get());
-        return 0;
+            deckCreatedCount++;
+        } else {
+            standing.setDeck(existingDeck);
+        }
     }
 
+
+    /** 
+     * Updates tournament stadings in DB with their match data coming from ABR.
+     * @param tournamentId id of tournament in question
+     * @param existingStandings list of existing stadings of the tournament in DB.
+     */
+    private void updateStandingsWithMatchData(int tournamentId, Set<Standing> existingStandings) {
+        Set<Standing> matches = abrBroker.loadMatches(tournamentId);
+        for (Standing match : matches) {
+            // Pair match with standings only if there is one match. (Team tournaments are excluded)
+            if (searcher.countStadingsByRankSide(existingStandings, match.getRank(), match.getIsRunner()) == 1) {
+                Standing standing = searcher.getStadingByRankSide(existingStandings, match.getRank(), match.getIsRunner());
+                // update stading if existing match data is different
+                if (!standing.areMatchDataIdentical(match)) {
+                        standing.copyFrom(match);
+                        standingRepository.save(standing);
+                        matchUpdatedCount++;
+                }
+            }
+        }
+    }
+
+    
+    /** 
+     * Updates counters of meta in DB.
+     * @param meta meta in question
+     */
     private void updateMetaCounts(Meta meta) {
         Long metaId = meta.getId();
         meta.setTournamentCount(metaRepository.countTournaments(metaId));
@@ -181,21 +272,5 @@ public class MetaOperations {
         meta.setMatchesCount(metaRepository.countMatches(metaId));
         meta.setLastUpdate(new Date());
         metaRepository.save(meta);
-    }
-
-    private int updateStandingsWithMatchData(int tournamentId, Set<Standing> existingStandings) {
-        int matchUpdatedCount = 0;
-        Set<Standing> matches = abrBroker.loadMatches(tournamentId);
-        for (Standing match : matches) {
-            // TODO: team tournaments with multiple players on same rank
-            Optional<Standing> existingStanding = existingStandings.stream().filter(x -> x.getRank() == match.getRank() && x.getIsRunner() == match.getIsRunner()).findFirst();
-            if (existingStanding.isPresent() && !existingStanding.get().areMatchDataIdentical(match)) {
-                    Standing standing = existingStanding.get();
-                    standing.copyFrom(match);
-                    standingRepository.save(standing);
-                    matchUpdatedCount++;
-            }
-        }
-        return matchUpdatedCount;
     }
 }
--- src/main/java/com/madarasz/knowthemeta/helper/Searcher.java
+++ src/main/java/com/madarasz/knowthemeta/helper/Searcher.java
@@ -4,23 +4,59 @@ import java.util.Collection;
 import java.util.Optional;
 
 import com.madarasz.knowthemeta.database.DOs.Card;
+import com.madarasz.knowthemeta.database.DOs.Deck;
+import com.madarasz.knowthemeta.database.DOs.Standing;
+import com.madarasz.knowthemeta.database.DOs.User;
 import com.madarasz.knowthemeta.database.DOs.relationships.CardInPack;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+/**
+ * Filters collections of KnowTheMeta entities.
+ * Returns first match. Returns null if no match exists.
+ */
+@Service
 public class Searcher {
     private final static Logger log = LoggerFactory.getLogger(Searcher.class);
 
     public Card getCardByCode(Collection<CardInPack> searchFrom, String cardCode) {
         Optional<CardInPack> result = searchFrom.stream().filter(x -> x.getCode().equals(cardCode)).findFirst();
-        if (!result.isPresent()) {
-            String errorMessage = "Could not find card for code: " + cardCode;
-            log.error(errorMessage);
-            // TODO: throw exception
+        if (result.isPresent()) {
+            return result.get().getCard();
         } 
-        return result.get().getCard();
+        String errorMessage = "Could not find card for code: " + cardCode;
+        log.error(errorMessage);
+        // TODO: throw exception
+        return null;
+    }
+
+    public Standing getStadingByRankSide(Collection<Standing> searchFrom, int rank, Boolean isRunner) {
+        Optional<Standing> result = searchFrom.stream().filter(x -> x.getRank() == rank && x.getIsRunner() == isRunner).findFirst();
+        if (result.isPresent()) {
+            return result.get();
+        }
+        return null;
+    }
+
+    public long countStadingsByRankSide(Collection<Standing> searchFrom, int rank, Boolean isRunner) {
+        return searchFrom.stream().filter(x -> x.getRank() == rank && x.getIsRunner() == isRunner).count();
+    }
+
+    public Deck getDeckById(Collection<Deck> searchFrom, int NetrunnerDBId) {
+        Optional<Deck> result = searchFrom.stream().filter(x -> x.getId() == NetrunnerDBId).findFirst();
+        if (result.isPresent()) {
+            return result.get();
+        }
+        return null;
+    }
+
+    public User getUserById(Collection<User> searchFrom, int NetrunnerDBId) {
+        Optional<User> result = searchFrom.stream().filter(x -> x.getUser_id() == NetrunnerDBId).findFirst();
+        if (result.isPresent()) {
+            return result.get();
+        }
+        return null;
     }
 }
